925c42c84e46c590e2f18e88934b422ee8a2c164
TheHellRuler <alienbrain4811@gmail.com>
2024-04-30 04:26:24 +0530

    Loading in /blog

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,18 +1,30 @@
 "use client"
 //page for detailed blog
 import fetchBlog,{blog} from '@/utils/blog'
-import { useSearchParams } from 'next/navigation'
+import { redirect, useSearchParams } from 'next/navigation'
 import Link from 'next/link'
-export default async function Page() {
+import { useState,useEffect } from 'react'
+export default function Page() {
   const params = useSearchParams()
-  const Blogs = await fetchBlog() as blog[]
+  const [Blogs, setBlogs] = useState<blog[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetchBlog();
+      setBlogs(data);
+      setLoading(false);
+    };
+    fetchData();
+  }, []);
   const id = params.get('id') || '' //Get blog slug from url parameter
-  const blog = Blogs.find((blog) => blog.slug === id)
-  if (blog === undefined) {
-    return(<p>Okda</p>)
+  const blog = Blogs.find((blog) => blog.slug === id) as blog
+  if (blog === undefined && !loading) {
+    redirect('/blogs')
   }
   return (
       <div className="container mx-auto py-8">
+        {loading?<p>Loading.. Please Wait</p>:
         <div className=" mx-auto flex flex-col lg:flex-row items-center">
           <img src={blog.urlToImage} alt={blog.title} className="w-full lg:w-3/5 h-auto rounded-lg mb-8 lg:mr-12 lg:mb-0" />
           <div className="lg:w-3/5">
@@ -20,7 +32,7 @@ export default async function Page() {
             <p className="text-gray-600 mt-4 text-lg">{blog.description}</p>
             <p className="text-gray-600 mt-4">{blog.content}</p>
           </div>
-        </div>
+        </div>}
       </div>
     )
   }
