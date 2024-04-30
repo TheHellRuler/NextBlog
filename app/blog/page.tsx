e77323c8a7fcd8ae6f45a3ed061cb9c176065a96
TheHellRuler <alienbrain4811@gmail.com>
2024-04-30 18:50:38 +0530

    Added Home button in /blog page

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -23,16 +23,25 @@ export default function Page() {
     redirect('/blogs')
   }
   return (
-      <div className="container mx-auto py-8">
-        {loading?<p>Loading.. Please Wait</p>:
-        <div className=" mx-auto flex flex-col lg:flex-row items-center">
-          <img src={blog.urlToImage} alt={blog.title} className="w-full lg:w-3/5 h-auto rounded-lg mb-8 lg:mr-12 lg:mb-0" />
+    <div className="container mx-auto py-8 relative">
+      {loading ? (
+        <p>Loading.. Please Wait</p>
+      ) : (
+        <div className="mx-auto flex flex-col lg:flex-row items-start">
+          {/* Home Button */}
+          <Link href="/blogs">
+              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" style={{ position: 'absolute', top: '1rem', left: '.5rem' }}>
+                Home
+              </button>
+              </Link>
+          <img src={blog.urlToImage} alt={blog.title} className="w-full lg:w-3/5 h-auto rounded-lg mb-8 lg:mr-12 lg:mb-0" style={{ marginTop: '3rem' }} />
           <div className="lg:w-3/5">
-          <h1 className="text-4xl font-bold mb-2">{blog.title}</h1>
+            <h1 className="text-4xl font-bold mb-2">{blog.title}</h1>
             <p className="text-gray-600 mt-4 text-lg">{blog.description}</p>
             <p className="text-gray-600 mt-4">{blog.content}</p>
           </div>
-        </div>}
-      </div>
+        </div>
+      )}
+    </div>
     )
   }
